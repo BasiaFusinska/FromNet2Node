149883c2353200eb9ab5b2956817d5a2ff5bc304
bfusion <barbara.fusinska@gmail.com>
2015-05-19 16:40:20 +0100

    reorganising web app to use index page

--- Net2NodeApp/app.js
+++ Net2NodeApp/app.js
@@ -7,7 +7,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var social = require('./routes/social');
+//var social = require('./routes/social');
 
 var app = express();
 
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/social', social);
+//app.use('/social', social);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

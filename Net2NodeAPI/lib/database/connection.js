0778b07c7a8976ea53ebbca62d22639ad81ff19e
bfusion <barbara.fusinska@gmail.com>
2015-05-20 12:42:22 +0100

    extracting connection to database to separate module

---
+++ Net2NodeAPI/lib/database/connection.js
@@ -0,0 +1,14 @@
+var pool = require('./pool')
+
+module.exports = function(query, params, callback){
+	pool.getConnection(function(err, connection) { 
+	    if (err) {
+	      console.log('ERROR: Unable to get connection due to ' + err.message);
+	    } else {
+        connection.query(query, params, function(err, result){
+          callback(err, result);
+        });
+        connection.release();
+      }
+  });
+}
--- Net2NodeAPI/routes/api.js
+++ Net2NodeAPI/routes/api.js
@@ -1,58 +1,38 @@
 var express = require('express');
 var router = express.Router();
-
-var pool = require('../lib/database/pool')
+var connection = require('../lib/database/connection')
 
 router.get('/events', function(req, res){
-  pool.getConnection(function(err, connection) { 
-	    if (err) {
-	      console.log('ERROR: Unable to get connection due to ' + err.message);
-	    } else {
-        connection.query('SELECT * from events', function(err, result){
-          if (err) {
-	          res.status(500).send(err);
-	        } else {
-            res.json(result);
-          }
-        });
-        connection.release();
+    connection('SELECT * from events', [], function(err, result) {
+      if (err) {
+        res.status(500).send(err);
       }
-  });
+      else {
+        res.json(result);
+      }
+    });
 });
 
 router.get('/events/:username', function(req, res){
-  pool.getConnection(function(err, connection) { 
-	    if (err) {
-	      console.log('ERROR: Unable to get connection due to ' + err.message);
-	    } else {
-        connection.query('SELECT * from events where username = ?', [req.params.username], function(err, result){
-          if (err) {
-	          res.status(500).send(err);
-	        } else {
-            res.json(result);
-          }
-        });
-        connection.release();
+    connection('SELECT * from events where username = ?', [req.params.username], function(err, result) {
+      if (err) {
+        res.status(500).send(err);
       }
-  });
+      else {
+        res.json(result);
+      }
+    });
 });
 
 router.post('/events/:username', function(req, res){
-    pool.getConnection(function(err, connection) { 
-	    if (err) {
-	      console.log('ERROR: Unable to get connection due to ' + err.message);
-	    } else {
-        console.log('event type: ' + req.query.eventType);
-	      connection.query('INSERT INTO events (event_type, payload, created_at, username) values (?, ?, ?, ?)',
-	        [req.query.eventType, JSON.stringify(req.body), new Date(), req.params.username], function(err, result) {
-	        if (err) {
-	          res.status(500).send(err);
-	        } else {
-	          res.status(200).send('Event is queued...');
-	        }
-	        connection.release();
-	      });
-	    }
+    connection('INSERT INTO events (event_type, payload, created_at, username) values (?, ?, ?, ?)',
+      [req.query.eventType, JSON.stringify(req.body), new Date(), req.params.username], function(err, result) {
+        if (err) {
+          res.status(500).send(err);
+        }
+        else {
+          res.status(200).send('Event is queued...');
+        }
     });
 });
 

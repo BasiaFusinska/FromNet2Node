e8e355509443d1aeb48066eda2b5fc268e3cefed
bfusion <barbara.fusinska@gmail.com>
2015-05-22 10:10:25 +0100

    tests refactoring

--- Net2NodeApp/tests/server/routeSpec.js
+++ Net2NodeApp/tests/server/routeSpec.js
@@ -1,19 +1,10 @@
-
 var index = require('../../routes/index.js');
 var routes = require('./route_helper.js').route_helper(index); 
 
-var event_finder = function(callback){
-    console.log('aaa');
-    callback(null, []);
-};
+describe('routes', function () {
 
-    describe('routes', function () {
-
-        it('GET /', function () {
-            routes.get('/');
-                console.log(routes.response);
-
-            expect(routes.response.viewName).toBe('');
-        });
-
+    it('GET /', function () {
+        routes.get('/');
+        expect(routes.response.viewName).toBe('');
     });
+});
